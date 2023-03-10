253406254f5bcfa3aa8684111ae04e8e329caef6
sospeter <sospeterkaranja02@gmail.com>
2023-03-10 11:44:41 +0300

    added assets and tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", 
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        whitesmoke: "#f2f3f5",
+        deepskyblue: "#00acff",
+        white: "#fff",
+        red: "#ff1930",
+        gray: { "100": "#212121", "200": "rgba(0, 0, 0, 0)" },
+        gainsboro: "#d8dadc",
+        crimson: "#e64646",
+        dimgray: "#666",
+        black: "#000",
+      },
+      fontFamily: { roboto: "Roboto",inter: "Inter", poppins: "Poppins" },
+      borderRadius: { "3xs": "10px", "mid-5": "17.5px" },
+    },
+    fontSize: {
+      xs: "0.69rem",
+      sm: "0.75rem",
+      base: "0.88rem",
+      lg: "1rem",
+      xl: "2.25rem",
+    },
+  },
+  plugins: [],
+}
