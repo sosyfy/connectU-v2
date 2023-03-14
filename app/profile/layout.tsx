864c7fcedc2b6b0bfc155592d57448d3360338d8
sospeter <sospeterkaranja02@gmail.com>
2023-03-14 22:56:02 +0300

    profile page

---
+++ app/profile/layout.tsx
@@ -0,0 +1,20 @@
+
+export const metadata = {
+    title: 'ConnectU',
+    description: "Built for the people by the people",
+  }
+  
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode,
+  }) {
+    return (
+      <html lang="en">
+        <body>
+          {children}
+        </body>
+      </html>
+    )
+  }
+  
---
+++ app/profile/page.tsx
@@ -0,0 +1,305 @@
+import LeftSide from "@/components/core/left-side";
+
+const Resume = () => {
+    return (
+        <main className="grid max-w-7xl grid-cols-12 gap-x-4 mx-auto pt-[6.5rem] pb-5 px-5 md:px-8 lg:px-0">
+            {/* <LeftSide /> */}
+
+
+            <div className="relative lg:col-span-9 md:col-span-8 bg-white w-full text-[1.25rem] text-black font-roboto">
+                 {/* profile  */}
+                 <div>
+                    <img
+                        className="absolute top-[10.5rem] left-[13rem] w-[16.5rem] h-[16.5rem] object-cover"
+                        alt="img"
+                        src="/img@2x.png"
+                    />
+                </div>
+                {/* experience  */}
+               
+                <div className="absolute top-[51.25rem] left-[40.5rem] w-[39rem] h-[59.25rem] overflow-hidden">
+                    <div className="absolute top-[calc(50%_+_314px)] left-[2rem] leading-[2rem] font-light inline-block w-[37rem]">
+                        The word résume comes from the French word résumer meaning to
+                        summarize. Leonardo da Vinci is credited with the first résumé, though
+                        his résume takes the form of a letter written about 1481–1482 to a
+                        potential employer, Ludovico Sforza.
+                    </div>
+                    <div className="absolute top-[calc(50%_+_266px)] left-[2rem] leading-[2rem] font-light">
+                        Company Name
+                    </div>
+                    <div className="absolute top-[calc(50%_+_234px)] left-[2rem] leading-[2rem] font-medium inline-block w-[21rem]">
+                        UX UI Designer
+                    </div>
+                    <div className="absolute top-[calc(50%_+_202px)] left-[2rem] leading-[2rem] font-light text-right">
+                        Yan 2022 - Yan 2022
+                    </div>
+                    <div className="absolute h-[0.84%] w-[1.28%] top-[72.57%] right-[98.72%] bottom-[26.58%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_+_10px)] left-[2rem] leading-[2rem] font-light inline-block w-[37rem]">
+                        The word résume comes from the French word résumer meaning to
+                        summarize. Leonardo da Vinci is credited with the first résumé, though
+                        his résume takes the form of a letter written about 1481–1482 to a
+                        potential employer, Ludovico Sforza.
+                    </div>
+                    <div className="absolute top-[calc(50%_-_38px)] left-[2rem] leading-[2rem] font-light">
+                        Company Name
+                    </div>
+                    <div className="absolute top-[calc(50%_-_70px)] left-[2rem] leading-[2rem] font-medium inline-block w-[21rem]">
+                        UX UI Designer
+                    </div>
+                    <div className="absolute top-[calc(50%_-_102px)] left-[2rem] leading-[2rem] font-light text-right">
+                        Yan 2022 - Yan 2022
+                    </div>
+                    <div className="absolute h-[0.84%] w-[1.28%] top-[40.51%] right-[98.72%] bottom-[58.65%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_-_294px)] left-[2rem] leading-[2rem] font-light inline-block w-[37rem]">
+                        The word résume comes from the French word résumer meaning to
+                        summarize. Leonardo da Vinci is credited with the first résumé, though
+                        his résume takes the form of a letter written about 1481–1482 to a
+                        potential employer, Ludovico Sforza.
+                    </div>
+                    <div className="absolute top-[calc(50%_-_342px)] left-[2rem] leading-[2rem] font-light">
+                        Company Name
+                    </div>
+                    <div className="absolute top-[calc(50%_-_374px)] left-[2rem] leading-[2rem] font-medium inline-block w-[21rem]">
+                        UX UI Designer
+                    </div>
+                    <div className="absolute top-[calc(50%_-_406px)] left-[2rem] leading-[2rem] font-light text-right">
+                        Yan 2022 - Yan 2022
+                    </div>
+                    <div className="absolute h-[0.84%] w-[1.28%] top-[8.44%] right-[98.72%] bottom-[90.72%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_-_474px)] left-[0rem] text-[1.75rem] leading-[2.25rem] font-medium">
+                        Experience
+                    </div>
+                </div>
+                <div className="absolute top-[48.19rem] left-[40.5rem] bg-lightgray w-[39rem] h-[0.06rem]" />
+                {/* about me  */}
+                <div className="absolute top-[33rem] left-[40.5rem] w-[39rem] h-[12.25rem] overflow-hidden">
+                    <div className="absolute top-[calc(50%_-_30px)] left-[0rem] leading-[2rem] font-light inline-block w-[39rem]">
+                        The word résume comes from the French word résumer meaning to
+                        summarize. Leonardo da Vinci is credited with the first résumé, though
+                        his résume takes the form of a letter written about 1481–1482 to a
+                        potential employer, Ludovico Sforza.
+                    </div>
+                    <div className="absolute top-[calc(50%_-_98px)] left-[0rem] text-[1.75rem] leading-[2.25rem] font-medium inline-block w-[39rem]">
+                        About Me
+                    </div>
+                </div>
+                <div className="absolute top-[29.94rem] left-[40.5rem] bg-lightgray w-[39rem] h-[0.06rem]" />
+                {/* info */}
+                <div>
+                    <div className="absolute top-[22rem] left-[44rem] leading-[2rem] font-light">
+                        San Francisco. California.
+                    </div>
+                    <div className="absolute top-[22rem] left-[40.5rem] w-[2rem] h-[2rem]">
+                        <div className="absolute top-[0rem] left-[0rem] w-[2rem] h-[2rem]" />
+                        <svg
+                            className="absolute top-[0.13rem] left-[0.31rem] w-[1.38rem] h-[1.75rem]"
+                            width="22"
+                            height="28"
+                            viewBox="0 0 22 28"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                                d="M11 1C16.3848 1 21 5.61522 21 11C21 16.3848 11 27 11 27C11 27 1 16.3848 1 11C1 5.61522 5.61522 1 11 1Z"
+                                stroke="#464646"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                        <div className="absolute top-[0.44rem] left-[0.63rem] rounded-lg box-border w-[0.75rem] h-[0.75rem] border-[2px] border-solid border-darkslategray" />
+                    </div>
+                    <div className="absolute top-[17.75rem] left-[40.5rem] text-[1.75rem] leading-[2.25rem] font-light inline-block w-[19rem]">
+                        UX UI Designer
+                    </div>
+                    <div className="absolute top-[13.5rem] left-[40.5rem] text-[3rem] leading-[3.25rem] font-medium">
+                        Ava Gregoraci
+                    </div>
+                </div>
+                {/* langauges */}
+                <div className="absolute top-[105.69rem] left-[10.5rem] w-[14.31rem] h-[8.75rem] overflow-hidden">
+                    <div className="absolute top-[calc(50%_+_38px)] left-[0rem] leading-[2rem]">
+                        <span className="font-medium">German</span>
+                        <span className="font-light"> - Native</span>
+                    </div>
+                    <div className="absolute top-[calc(50%_-_2px)] left-[0rem] leading-[2rem]">
+                        <span className="font-medium">English</span>
+                        <span className="font-light"> - Conversational</span>
+                    </div>
+                    <div className="absolute top-[calc(50%_-_70px)] left-[0rem] text-[1.75rem] leading-[2.25rem] font-medium">
+                        Languages
+                    </div>
+                </div>
+
+                <div className="absolute top-[102.69rem] left-[10.5rem] bg-lightgray w-[21.5rem] h-[0.06rem]" />
+                {/* education  */}
+                <div className="absolute top-[73.5rem] left-[10.5rem] w-[19rem] h-[26.25rem] overflow-hidden">
+                    <div className="absolute top-[calc(50%_+_178px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        University name
+                    </div>
+                    <div className="absolute top-[calc(50%_+_146px)] left-[2rem] leading-[2rem] font-medium inline-block w-[17rem]">
+                        Animation
+                    </div>
+                    <div className="absolute top-[calc(50%_+_114px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        2022 - 2023
+                    </div>
+                    <div className="absolute h-[1.9%] w-[2.63%] top-[80%] right-[97.37%] bottom-[18.1%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_+_50px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        University name
+                    </div>
+                    <div className="absolute top-[calc(50%_+_18px)] left-[2rem] leading-[2rem] font-medium inline-block w-[17rem]">
+                        Animation
+                    </div>
+                    <div className="absolute top-[calc(50%_-_14px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        2022 - 2023
+                    </div>
+                    <div className="absolute h-[1.9%] w-[2.63%] top-[49.52%] right-[97.37%] bottom-[48.57%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_-_78px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        University name
+                    </div>
+                    <div className="absolute top-[calc(50%_-_110px)] left-[2rem] leading-[2rem] font-medium inline-block w-[17rem]">
+                        Branding
+                    </div>
+                    <div className="absolute top-[calc(50%_-_142px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        2022 - 2023
+                    </div>
+                    <div className="absolute h-[1.9%] w-[2.63%] top-[19.05%] right-[97.37%] bottom-[79.05%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_-_210px)] left-[0rem] text-[1.75rem] leading-[2.25rem] font-medium">
+                        Education
+                    </div>
+                </div>
+                <div className="absolute top-[70.44rem] left-[10.5rem] bg-lightgray w-[21.5rem] h-[0.06rem]" />
+                {/* skills  */}
+                <div className="absolute top-[51.25rem] left-[10.5rem] w-[19rem] h-[16.25rem] overflow-hidden">
+                    <div className="absolute top-[calc(50%_+_98px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        Animation
+                    </div>
+                    <div className="absolute h-[3.08%] w-[2.63%] top-[92.31%] right-[97.37%] bottom-[4.62%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_+_58px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        Graphic Design
+                    </div>
+                    <div className="absolute h-[3.08%] w-[2.63%] top-[76.92%] right-[97.37%] bottom-[20%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_+_18px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        Branding
+                    </div>
+                    <div className="absolute h-[3.08%] w-[2.63%] top-[61.54%] right-[97.37%] bottom-[35.38%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_-_22px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        Web Design
+                    </div>
+                    <div className="absolute h-[3.08%] w-[2.63%] top-[46.15%] right-[97.37%] bottom-[50.77%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_-_62px)] left-[2rem] leading-[2rem] font-light inline-block w-[17rem]">
+                        UX UI Design
+                    </div>
+                    <div className="absolute h-[3.08%] w-[2.63%] top-[30.77%] right-[97.37%] bottom-[66.15%] left-[0%] rounded-lg bg-black" />
+                    <div className="absolute top-[calc(50%_-_130px)] left-[0rem] text-[1.75rem] leading-[2.25rem] font-medium">
+                        Skills
+                    </div>
+                </div>
+                <div className="absolute top-[48.19rem] left-[10.5rem] bg-lightgray w-[21.5rem] h-[0.06rem]" />
+                {/* contact */}
+                <div className="absolute top-[33rem] left-[10.5rem] w-[22rem] h-[11.25rem] overflow-hidden">
+                    <div className="absolute top-[calc(50%_+_58px)] left-[3.5rem] leading-[2rem] font-light inline-block w-[18.5rem]">
+                        www.ava.gregoraci.com
+                    </div>
+                    <svg
+                        className="absolute h-[17.78%] w-[9.09%] top-[82.22%] right-[90.91%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 32 32"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <circle
+                            cx="16"
+                            cy="16"
+                            r="12"
+                            stroke="#464646"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <ellipse
+                            cx="16"
+                            cy="16"
+                            rx="6"
+                            ry="12"
+                            stroke="#464646"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M27 21L5 21"
+                            stroke="#464646"
+                            stroke-width="2"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M27 11L5 11"
+                            stroke="#464646"
+                            stroke-width="2"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                    <div className="absolute top-[calc(50%_+_18px)] left-[3.5rem] leading-[2rem] font-light inline-block w-[18.5rem]">
+                        ava.gregoraci@gmail.com
+                    </div>
+                    <div className="absolute h-[17.78%] w-[9.09%] top-[60%] right-[90.91%] bottom-[22.22%] left-[0%]">
+                        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%]" />
+                        <div className="absolute h-[62.5%] w-[81.25%] top-[18.75%] right-[9.38%] bottom-[18.75%] left-[9.38%] rounded-sm box-border border-[2px] border-solid border-darkslategray" />
+                        <svg
+                            width="18"
+                            height="8"
+                            className="absolute h-1/4 w-[56.25%] top-[31.25%] right-[21.87%] bottom-[43.75%] left-[21.87%] max-w-full overflow-hidden max-h-full"
+                            viewBox="0 0 18 8"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M1 1L9 7L17 1"
+                                stroke="#464646"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                    </div>
+                    <div className="absolute top-[calc(50%_-_22px)] left-[3.5rem] leading-[2rem] font-light inline-block w-[18.5rem]">
+                        +1 415 844 4278
+                    </div>
+                    <div className="absolute h-[17.78%] w-[9.09%] top-[37.78%] right-[90.91%] bottom-[44.44%] left-[0%]">
+                        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%]" />
+                        <svg
+                            width="27"
+                            height="27"
+                            className="absolute h-[79.39%] w-[79.39%] top-[11.44%] right-[9.26%] bottom-[9.17%] left-[11.35%] max-w-full overflow-hidden max-h-full"
+                            viewBox="0 0 27 27"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                                d="M4.24049 1.94601L5.46792 1.70053C6.32077 1.52996 7.18609 1.93011 7.60847 2.6904L9.70237 6.45942C10.136 7.24001 9.99969 8.2135 9.36827 8.84492L7.95858 10.2546C7.30977 10.9042 7.18657 11.9121 7.66039 12.6985C8.45555 14.0206 9.38117 15.2096 10.4373 16.2657C11.4933 17.3218 12.6816 18.2466 14.0019 19.0401C14.7884 19.5118 15.795 19.388 16.4439 18.7399L17.8535 17.3302C18.485 16.6988 19.4585 16.5624 20.239 16.9961L24.0081 19.09C24.7684 19.5124 25.1685 20.3777 24.9979 21.2305L24.7525 22.458C24.6351 23.0442 24.2617 23.5472 23.7337 23.8275C18.7799 26.468 13.4042 24.8895 7.60658 19.0919C1.80898 13.2943 0.230442 7.91858 2.87098 2.96478C3.1513 2.43674 3.65429 2.06335 4.24049 1.94601Z"
+                                stroke="#464646"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                    </div>
+                    <div className="absolute top-[calc(50%_-_90px)] left-[0rem] text-[1.75rem] leading-[2.25rem] font-medium">
+                        Contact
+                    </div>
+                </div>
+                <div className="absolute top-[29.94rem] left-[10.5rem] bg-lightgray w-[21.5rem] h-[0.06rem]" />
+
+            </div>
+
+        </main>
+    );
+};
+
+export default Resume;
