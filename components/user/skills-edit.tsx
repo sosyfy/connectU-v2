b5a82e385918922455e467160e6f81ba04fdfb20
sospeter <sospeterkaranja02@gmail.com>
2023-03-26 17:29:00 +0300

    Feat: Skills Edit

---
+++ components/user/skills-edit.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import { useState } from "react";
+
+interface EditSkillsProps {
+    prevSkills?: string[],
+    onSave: any,
+    edit: boolean,
+    loading: boolean,
+    closeModal: any 
+}
+
+export function EditSkills({ prevSkills, onSave, edit, loading, closeModal }: EditSkillsProps) {
+    const [skills, setSkills] = useState<string[] | any>(edit ? prevSkills : [])
+    const [inputValue, setInputValue] = useState("");
+
+    function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === "Enter" || event.key === ",") {
+            event.preventDefault();
+            addSkill(inputValue.trim());
+            setInputValue("");
+        }
+    }
+
+    function addSkill(skillName: string) {
+        if (skillName && !skills?.includes(skillName)) {
+            setSkills([...skills, skillName]);
+        }
+    }
+
+    function removeSkill(skillName: string) {
+        setSkills(skills.filter((skill: string) => skill !== skillName));
+    }
+
+    return (
+        <div className="grid gap-2">
+            <div className="flex flex-wrap items-center gap-2">
+                {skills.map((skillName: string) => (
+                    <div
+                        key={skillName}
+                        className='bg-transparent border-2 flex items-center gap-1 border-black/60 shadow-inner text-black/60 px-[2rem] py-1 rounded-full'
+                    >
+                        <span> {skillName}  </span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24"
+                            strokeWidth={1.5} stroke="currentColor"
+                            className="w-4 h-4 cursor-pointer"
+                            onClick={() => removeSkill(skillName)}
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+
+                    </div>
+                ))}
+            </div>
+            <input
+                type="text"
+                placeholder="Add a skill"
+                value={inputValue}
+                onChange={(event) => setInputValue(event.target.value)}
+                onKeyDown={handleKeyPress}
+                className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400"
+            />
+
+            <div className="flex justify-end w-full gap-3">
+                <button
+                    onClick={()=>closeModal()}
+                    className="flex items-center justify-center whitespace-nowrap rounded-lg mt-5 bg-gray-500 px-6 pt-2.5 pb-2 text-xs font-semibold uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200 motion-reduce:transition-none"
+                >
+                    close
+                </button>
+                <button
+                    disabled={loading}
+                    type="button"
+                    className="flex items-center justify-center whitespace-nowrap rounded-lg mt-5 bg-deepskyblue px-6 pt-2.5 pb-2 text-xs font-semibold uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200 motion-reduce:transition-none"
+                    onClick={(e) => onSave(skills)}
+                >
+                    {loading ? (
+                        <svg
+                            aria-hidden="true"
+                            role="status"
+                            className="inline w-5 h-5 mr-5 text-gray-200 animate-spin"
+                            viewBox="0 0 100 101"
+                            fill="#fff"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                            />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="#1C64F2"
+                            />
+                        </svg>
+                    ) : (
+                        <span> create post</span>
+                    )}
+
+                </button>
+            </div>
+
+        </div>
+    );
+}
--- components/user/skills.tsx
+++ components/user/skills.tsx
@@ -1,45 +1,81 @@
 "use client"
 
+import { useState } from "react"
+import { EditSkills } from "./skills-edit"
+
 type Props = {
     user: User,
     loggedInUser: User | undefined
 }
- 
+
 export default function Skills({ user, loggedInUser }: Props) {
+    const [skills, setSkills] = useState<string[]>(user.skills)
+    const [showModal, setShowModal] = useState(false);
+    const [edit, setEdit] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const openEditModal = () => {
+        setShowModal(true);
+        setEdit(true);
+    }
+    const openCreateModal = () => {
+        setShowModal(true);
+        setEdit(false);
+    }
+
+    const handleSubmit = (skillsList: any)=>{
+        setShowModal(false); 
+
+    }
+
     return (
         <section className='pb-2 bg-white rounded-lg shadow border-dimgray'>
             {/* top part  */}
             <div className="p-[1.2rem] pb-0">
                 <div className="flex items-center justify-between">
                     <h2 className="flex-1 font-semibold text-textblack-100">SKills</h2>
-                   
-                    {loggedInUser?._id === user?._id && (
-                    <div className="flex items-center gap-2 icons">
 
-                        <button className="grid p-2 rounded-full focus:outline-none hover:bg-zinc-200 place-content-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                            </svg>
-                        </button>
-                        <button className="grid p-2 rounded-full focus:outline-none hover:bg-zinc-200 place-content-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                    </svg>
-                </button>
-                    </div>
+                    {loggedInUser?._id === user?._id && (
+                        <div className="flex items-center gap-2 icons">
+
+                            <button onClick={openCreateModal} className="grid p-2 rounded-full focus:outline-none hover:bg-zinc-200 place-content-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                                </svg>
+                            </button>
+                            <button onClick={openEditModal} className="grid p-2 rounded-full focus:outline-none hover:bg-zinc-200 place-content-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                </svg>
+                            </button>
+                        </div>
                     )}
                 </div>
             </div>
             {/* things    */}
             <div className="grid p-[1.2rem] relative">
                 <div className="flex flex-wrap gap-4">
-                    { user.skills.map((skill, index)=>(
-                    <div  key={skill + index} className='bg-transparent border-2 border-black/60 shadow-inner text-black/60 px-[1.6rem] py-1 rounded-full'>
-                       { skill }
-                    </div>
+                    {skills.map((skill, index) => (
+                        <div key={skill + index} className='bg-transparent border-2 border-black/60 shadow-inner text-black/60 px-[1.6rem] py-1 rounded-full'>
+                            {skill}
+                        </div>
                     ))}
                 </div>
-              
+
+                {showModal &&
+                    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+                        <div className="relative w-auto max-w-sm px-4 py-6 mx-auto bg-gray-300 rounded-lg shadow">
+                            
+                            <EditSkills
+                                edit={edit}
+                                prevSkills={skills}
+                                loading={loading}
+                                onSave={handleSubmit}
+                                closeModal={()=> setShowModal(false)}
+                            />
+                        </div>
+                    </div>
+                }
             </div>
 
         </section>
