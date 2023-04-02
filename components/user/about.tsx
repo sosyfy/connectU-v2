0c79920f5003caf761a1bc5046f49186b4c89ef7
Sospeter Karanja <sospeterkaranja02@gmail.com>
2023-04-03 02:19:53 +0300

    Fix: About not responding

--- components/user/about.tsx
+++ components/user/about.tsx
@@ -57,7 +57,7 @@ export default function About({ user, loggedInUser , token  }: Props) {
                     <h2 className="flex-1 font-semibold text-textblack-100">About</h2>
                     {loggedInUser?._id === user?._id && (
                         <div className="flex items-center gap-2 icons">
-                            <button onClick={() => openEditModal(user?.about)} className="grid p-2 rounded-full focus:outline-none hover:bg-zinc-200 place-content-center">
+                            <button onClick={() => openEditModal(aboutData)} className="grid p-2 rounded-full focus:outline-none hover:bg-zinc-200 place-content-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                                 </svg>
