cbb85fe2309804abf677b4e0c696930ca79a4427
Sospeter Karanja <98221088+sosyfy@users.noreply.github.com>
2023-09-21 08:09:54 +0300

    added default user

--- app/(auth)/login/login.tsx
+++ app/(auth)/login/login.tsx
@@ -6,8 +6,8 @@ import React, { useState } from 'react'
 
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("edwin@gmail.com");
+    const [password, setPassword] = useState("123456");
     const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (event: any) => {
